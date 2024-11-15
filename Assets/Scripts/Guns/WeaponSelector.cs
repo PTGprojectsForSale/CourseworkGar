8c89e06e5ac009a79ee207e7d5e00d44c92ff28c
VadimLNN <svobodolubsev@inbox.ru>
2024-11-15 16:19:51 +0700

    добавил скрипты из 6 лабы

---
+++ Assets/Scripts/Guns/WeaponSelector.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponSelector : MonoBehaviour
+{
+    public Transform weaponHolder;
+
+    int selectedWeaponIndex = 0;
+
+    void Start() => hideWeapon();
+    public CWeapon selectNextWeapon()
+    {
+        hideWeapon();
+
+        selectedWeaponIndex++;
+
+        if (selectedWeaponIndex > weaponHolder.childCount-1)
+            selectedWeaponIndex = 0;
+
+        weaponHolder.GetChild(selectedWeaponIndex).gameObject.SetActive(true);
+        return weaponHolder.GetChild(selectedWeaponIndex).gameObject.GetComponent<CWeapon>();
+    }
+
+    public CWeapon selectPrevWeapon()
+    {
+        hideWeapon();
+
+        selectedWeaponIndex--;
+
+        if (selectedWeaponIndex < 0)
+            selectedWeaponIndex = weaponHolder.childCount - 1;
+
+        weaponHolder.GetChild(selectedWeaponIndex).gameObject.SetActive(true);
+        return weaponHolder.GetChild(selectedWeaponIndex).gameObject.GetComponent<CWeapon>();
+    }
+
+    public CWeapon selectWeaponByIndex(int ind)
+    {
+        hideWeapon();
+
+        if (ind > -1 && ind <= weaponHolder.childCount)
+        {
+            selectedWeaponIndex = ind;
+
+            weaponHolder.GetChild(selectedWeaponIndex).gameObject.SetActive(true);
+            return weaponHolder.GetChild(selectedWeaponIndex).gameObject.GetComponent<CWeapon>();
+        }
+
+        return null;
+    }
+
+    void hideWeapon()
+    {
+        foreach (Transform child in weaponHolder)
+            child.gameObject.SetActive(false);
+    }
+
+    public CWeapon getSelectedWeapon()
+    {
+        return weaponHolder.GetChild(selectedWeaponIndex).gameObject.GetComponent<CWeapon>();
+    }
+}
